b5abd0f13758343e588175bdc769d0fd211c11fa
SamuelChorvat <36892499+SamuelChorvat@users.noreply.github.com>
2021-08-05 12:35:29 +0100

    add option to logout after login

--- src/main/java/com/kainos/ea/ui/TerminalInterface.java
+++ src/main/java/com/kainos/ea/ui/TerminalInterface.java
@@ -11,26 +11,26 @@ public class TerminalInterface {
     public static void main(String[] args) {
         TerminalInterface ui = new TerminalInterface();
         ui.loginProcess();
-
-
-
-        System.out.println(ui.getEmployeeID());
-        System.out.println(ui.getEmployeePassword());
-
     }
 
     public void loginProcess() {
-        while (!isLoggedIn()) {
-            try {
-                requestLogin();
-                setLoggedIn(true);
-            } catch (NumberFormatException e) {
-                System.out.println("EmployeedID MUST BE AN INTEGER.");
-            }
+        try {
+            requestLogin();
+        } catch (NumberFormatException e) {
+            System.out.println("EmployeeID MUST BE AN INTEGER.");
+            return;
+        }
+
+        if (!validateLogin()) {
+            System.out.println("Invalid EmployeeID or Password");
+            return;
+        } else {
+            setLoggedIn(true);
+            loggedInProcess();
         }
     }
 
-    public void requestLogin() {
+    private void requestLogin() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your EmpployeeID: ");
         setEmployeeID(Integer.parseInt(scanner.nextLine()));
@@ -38,6 +38,38 @@ public class TerminalInterface {
         setEmployeePassword(scanner.nextLine());
     }
 
+    //Stub method
+    private boolean validateLogin() {
+        return true;
+    }
+
+    private void loggedInProcess() {
+        if (!loggedIn) {
+            return;
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        int selectedOption = 0;
+
+        System.out.println();
+        System.out.println("What would you like to do Employee_" + getEmployeeID() +" ?");
+        System.out.println("1.Logout");
+
+        try {
+            selectedOption = Integer.parseInt(scanner.nextLine());
+        } catch (NumberFormatException e){
+            System.out.println("Bad input!");
+            return;
+        }
+
+        if (selectedOption == 1) {
+            setLoggedIn(false);
+            loginProcess();
+        } else {
+            System.out.println("No option for number " + selectedOption + ", try again!");
+            return;
+        }
+    }
 
     public int getEmployeeID() {
         return employeeID;
